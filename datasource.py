1840bb046d7b6d6b44b23853a0b6580a3d54dc46
ashoke saha <ashokesaha@ashokes-MacBook-Pro.local>
2025-09-09 19:05:30 +0530

    adding more files

--- datasource.py
+++ datasource.py
@@ -1,7 +1,10 @@
 import sys
 import re
+import logging
 import gensim
 import preprocess
+from gensim import models
+from gensim.models import LsiModel
 
 from io import StringIO
 from collections import Counter
@@ -38,29 +41,63 @@ class MyCorpus :
     def TOKEN(self,id) :
         return self.dictionary[id]
 
+    def DICTIONARY(self) :
+        return self.dictionary
     
 
 
 
 
 if __name__ == '__main__' :
-    #fs = FileSource('Food/small.csv')
-    #dict = gensim.corpora.Dictionary(fs)
-    #print('')
-    #print(dict)
-    #print(dict.token2id)
-    #print('\n\n')
-    #print(dict.cfs)
-    #print('\n\n')
-    #print(dict.dfs)
+    #logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO) 
+
+    FS = FileSource('rfc793.txt')
+    MC = MyCorpus('rfc793.txt')
+    bows = MC.BOW('rfc793.txt')
+
+    tfidf = models.TfidfModel(bows) 
+    tfidf_corpus = tfidf[bows]
+
+    #lsimodel = LsiModel(bows[:100], id2word=MC.DICTIONARY(), num_topics=8)
+    #vectors = lsimodel[bows[150:155]]
+    lsimodel = LsiModel(tfidf_corpus[:100], id2word=MC.DICTIONARY(), num_topics=8)
+    vectors = lsimodel[tfidf_corpus[150:155]]
+    topics = lsimodel.print_topics(8)
+    #print(topics)
 
 
-    MC = MyCorpus('Food/small.csv')
-    bows = MC.BOW('bow.txt')
-    for bow in bows :
-        print(bow)
-        for b in bow :
-            print('({},{})'.format( MC.TOKEN(b[0]), b[1]),  end=' ')
-        print('')
+    for line in FS :
+        print(line)
+    print('\n\n')
+
+    dict = MC.DICTIONARY()
+    print(dict.token2id) 
+    print('\n\n')
+    print(dict.dfs) 
+    print('\n\n')
+    print(dict.cfs) 
+
+
+    #print(vectors)
+    #for v in vectors :
+    #    print(v)
+    #print('\n\n')
+
+    #for ix in range(8) :
+    #    lsimodel.show_topic(ix)
+    #    print('---- --- ---- -- -- - - --- -------')
+
+
+    #print(bows)
+    #print('\n')
+    #print(tfidf_corpus)
+    #for doc in tfidf_corpus :
+    #    print(doc)
+
+    #for fs,bow in zip(FS,bows) :
+    #    print(fs)
+    #    for b in bow :
+    #        print('({},{})'.format( MC.TOKEN(b[0]), b[1]),  end=' ')
+    #    print('')
 
 
---
+++ simili.py
@@ -0,0 +1,79 @@
+from collections import defaultdict
+from gensim import corpora
+from gensim import models
+from gensim import similarities
+
+
+import sys
+
+
+
+
+documents = [
+    "Human machine interface for lab abc computer applications",
+    "A survey of user opinion of computer system response time",
+    "The EPS user interface management system",
+    "System and human system engineering testing of EPS",
+    "Relation of user perceived response time to error measurement",
+    "The generation of random binary unordered trees",
+    "The intersection graph of paths in trees",
+    "Graph minors IV Widths of trees and well quasi ordering",
+    "Graph minors A survey",
+]
+
+doc = "Human computer interaction"
+
+
+
+
+FileSources = ['Food/biriyani.txt', 'Food/burrito.txt', 'Food/butter_chicken.txt',
+               'Food/cake.txt', 'Food/chowmein.txt', 'Food/dosa.txt', 'Food/lasagne.txt' ]
+documents = []
+for ix, filename in enumerate(FileSources) :
+    with open(filename,'r') as fp :
+        documents.append(fp.read()) 
+
+
+doc = 'spicy india chicken dish'
+doc = 'dish with egg'
+doc = 'baked dish  with batter and egg'
+
+
+
+stoplist = set('for a of the and to in'.split())
+texts = [
+    [word for word in document.lower().split() if word not in stoplist]
+    for document in documents
+]
+
+frequency = defaultdict(int)
+for text in texts:
+    for token in text:
+        frequency[token] += 1
+
+texts = [
+    [token for token in text if frequency[token] > 1]
+    for text in texts
+]
+
+dictionary = corpora.Dictionary(texts)
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+tfidf = models.TfidfModel(corpus)
+corpus = tfidf[corpus]
+
+lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=4)
+
+vec_bow = dictionary.doc2bow(doc.lower().split())
+vec_lsi = lsi[vec_bow]  # convert the query to LSI space
+
+index = similarities.MatrixSimilarity(lsi[corpus])  # transform corpus to LSI space and index it
+
+sims = index[vec_lsi]  # perform a similarity query against the corpus
+sims = sorted(enumerate(sims), key=lambda item: -item[1])
+
+print('Query: {}'.format(doc))
+for doc_position, doc_score in sims:
+    print('{0:<32}    {1}'.format(FileSources[doc_position], doc_score))
+    #print(doc_position, doc_score, documents[doc_position])
+
